80e1ea80ebef8f1d51defc4082cfc0f9e77aaa6f
ColeMcdaniel <32074076+ColeMcdaniel@users.noreply.github.com>
2018-04-09 12:21:58 -0500

    Prototype
    need to fix endless loop bug of floors duplicating itself, but not giving a gap for the player to jump over.

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour 
+{
+	public GameObject theGround;
+	public Transform generationPoint;
+	public float distanceBetween;
+
+	private float groundWidth;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		groundWidth = theGround.GetComponent<BoxCollider2D> ().size.x;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if (transform.position.x < generationPoint.position.x) 
+		{
+			transform.position = new Vector3 (transform.position.x + groundWidth + distanceBetween, transform.position.y, transform.position.z);
+
+			Instantiate (theGround, transform.position, transform.rotation);
+		}
+	}
+}
